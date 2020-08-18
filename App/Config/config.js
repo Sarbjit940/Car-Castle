5fa79391e5bc43e1b97d3e5d739112d85b2a586f
Sarbjit <singhsarbjit940@gmail.com>
2020-08-18 12:05:35 +0530

    config

---
+++ App/Config/config.js
@@ -0,0 +1,8 @@
+//Connection
+global.HOST = '127.0.0.1';
+global.USER = 'root';
+global.PASSWORD = 'sarbjit@';
+global.DATABASE = 'car-castle';
+
+
+global.API_KEY = 's3cr3tp4sswo4rd@@12345HJLP$%$Geenejdiddm';
